c336c9bbae387dc26e935bab17fcf62544faffde
Abbash Ali <abbasali6021@gmail.com>
2023-09-15 00:17:01 +0530

    error handling

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { Link, useNavigate, useRouteError } from 'react-router-dom'
+
+import {HomeIcon, ArrowUturnLeftIcon} from '@heroicons/react/24/solid'
 
 export default function Error() {
+  const error = useRouteError()
+  const navigate = useNavigate()
+  console.log(error)
   return (
-    <div>Error, the route doesn't exist !</div>
+    <div className="error">
+      <h1>Oops, there's a problem!</h1>
+      <p>{error.message || error.statusText}</p>
+      <div className="flex-md">
+        <button className='btn btn--dark' onClick={() => navigate(-1)}>
+          <ArrowUturnLeftIcon width={20}/>
+          <span>Go Back</span>
+        </button>
+        <Link to="/" className='btn btn--dark'>
+          <HomeIcon width={20}/>
+          <span>Go Home</span>
+        </Link>
+      </div>
+    </div>
   )
 }
